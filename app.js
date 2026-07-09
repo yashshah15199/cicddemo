55ce1963c977f0a9d2c0cf75dd0a2356ecc4ddd0
yashshah15199 <yashshah15199@gmail.com>
2026-07-09 10:12:21 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let express = require('express');
+let app = express();
+let port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
